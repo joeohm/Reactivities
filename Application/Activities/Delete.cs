85667852aa84d997c15e1784b42c2dbc4d21f2fe
Joel Öhman <joel.ohman@entryevent.se>
2024-11-16 11:35:23 +0100

    Error handling in API and client

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -7,27 +8,31 @@ namespace Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
             {
                 _context = context;
             }
-            public async Task Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
 
+                if (activity == null) return null;
+
                 _context.Remove(activity);
 
-                // _mapper.Map(request.Activity, activity);
+                var result = await _context.SaveChangesAsync() > 0;
 
-                await _context.SaveChangesAsync();
+                if (!result) return Result<Unit>.Failure("Failed to delete the activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
